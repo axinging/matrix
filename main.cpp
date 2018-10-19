c13bc2973fb2ef27f467284ee08877fbbe49e4a4
Xu Xing <xing.xu@intel.com>
2018-10-19 15:39:11 +0800

    Add main.cpp for nornal

--- main.cpp
+++ main.cpp
@@ -33,8 +33,6 @@ Matrix4 setOrthoFrustum(float l, float r, float b, float t, float n, float f);
 // constants
 const float DEG2RAD = 3.141593f / 180;
 
-
-
 float GetZn(float n, float f, float Ze) {
   float f_plus_n = f + n;
   float f_sub_n = f - n;
@@ -44,6 +42,17 @@ float GetZn(float n, float f, float Ze) {
 
   float Zn = -1 * (A * Ze + B) / Ze;
   return Zn;
+}
+
+void printMatrix4(const Matrix4& mat4) {
+  std::cout << " " << mat4[0] << ", " << mat4[1] << ", " << mat4[2] << ", "
+            << mat4[3] << std::endl;
+  std::cout << " " << mat4[4] << ", " << mat4[5] << ", " << mat4[6] << ", "
+            << mat4[7] << std::endl;
+  std::cout << " " << mat4[8] << ", " << mat4[9] << ", " << mat4[10] << ", "
+            << mat4[11] << std::endl;
+  std::cout << " " << mat4[12] << ", " << mat4[13] << ", " << mat4[14] << ", "
+            << mat4[15] << std::endl;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -58,9 +67,10 @@ int main(int argc, char** argv) {
 
   {
     Matrix4 matProject = setFrustum(-100, 100, -100, 100, 1, 100);
+    printMatrix4(matProject);
     Vector4 point(-200, 200, -2, 1);
     Vector4 projectedPoint = matProject * point;
-    /*projectedPoint (-1, 1, -4.06061, -2)*/
+    /*projectedPoint (-2, 2, 0.0202022, 2), not normalized*/
     std::cout << "projectedPoint: " << projectedPoint << std::endl;
     std::cout << "GetZn: " << GetZn(1, 100, -2) << std::endl;
   }
@@ -71,20 +81,18 @@ int main(int argc, char** argv) {
 // set a perspective frustum with 6 params similar to glFrustum()
 // (left, right, bottom, top, near, far)
 ///////////////////////////////////////////////////////////////////////////////
-Matrix4 setFrustum(float l, float r, float b, float t, float n, float f)
-{
-    Matrix4 mat;
-    mat[0]  =  2 * n / (r - l);
-    mat[5]  =  2 * n / (t - b);
-    mat[8]  =  (r + l) / (r - l);
-    mat[9]  =  (t + b) / (t - b);
-    mat[10] = -(f + n) / (f - n);
-    mat[11] = -1;
-    mat[14] = -(2 * f * n) / (f - n);
-    mat[15] =  0;
-    return mat;
+Matrix4 setFrustum(float l, float r, float b, float t, float n, float f) {
+  Matrix4 mat;
+  mat[0] = 2 * n / (r - l);
+  mat[5] = 2 * n / (t - b);
+  mat[8] = (r + l) / (r - l);
+  mat[9] = (t + b) / (t - b);
+  mat[10] = -(f + n) / (f - n);
+  mat[11] = -1;
+  mat[14] = -(2 * f * n) / (f - n);
+  mat[15] = 0;
+  return mat;
 }
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // set a symmetric perspective frustum with 4 params similar to gluPerspective
